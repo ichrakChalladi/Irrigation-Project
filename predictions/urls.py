4c1b27d3320b17ca2e3ab474398b8e81e00c6c2e
ichrak challadia <ichrak.benchalladia@esprit.tn>
2024-11-05 14:56:09 +0100

    fix

--- predictions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import IrrigationPredictionView, index
-
-
-urlpatterns = [
-    path('', index, name='predict'),
-    path('predict/', IrrigationPredictionView.as_view(), name='irrigation_prediction'),
-]
